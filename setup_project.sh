ff10b2170e65df534ab73f46a7c65ecc69d9ff59
sanketbhalsing25 <your_email@gmail.com>
2026-01-02 14:53:39 +0530

    folder stucture added

---
+++ setup_project.sh
@@ -0,0 +1,11 @@
+mkdir -p src
+mkdir -p tests
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch tests/trials.ipynb
+touch requirements.txt
